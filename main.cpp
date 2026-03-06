925198f2ba6910ab425cb867b4e6c6ee0ccecfcf
Yamin  Mahmood <128117506+yaminyt@users.noreply.github.com>
2026-03-05 19:01:39 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,5 +20,20 @@ int main()
    std::cout << "Deck after shuffle:" << std::endl;
    std::cout << d << std::endl;
 
+   // deal 24 cards into a new deck
+   deck table;
+
+   for (int i = 0; i < 24; i++)
+   {
+      d_node *c = d.deal();
+      table.replace(c);
+   }
+
+   std::cout << "Cards dealt to table:" << std::endl;
+   std::cout << table << std::endl;
+
+   std::cout << "Remaining deck:" << std::endl;
+   std::cout << d << std::endl;
+
    return 0;
 }
